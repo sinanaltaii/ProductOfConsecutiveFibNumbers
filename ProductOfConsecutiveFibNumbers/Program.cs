27784972a519bc7276b70bb75424a7fa54fbe564
Sinan Altaii <sinan.altaii@live.se>
2021-02-22 00:51:26 +0100

    solved it with better algorithm

--- ProductOfConsecutiveFibNumbers/Program.cs
+++ ProductOfConsecutiveFibNumbers/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ProductOfConsecutiveFibNumbers
 {
@@ -7,64 +7,27 @@ namespace ProductOfConsecutiveFibNumbers
 	{
 		static void Main()
 		{
+			var stopWatch = Stopwatch.StartNew();
 			var result = ProductFib(4895);
+			stopWatch.Stop();
+			Console.WriteLine($"Ticks: {stopWatch.ElapsedTicks}");
 			Console.WriteLine(string.Join(", ", result).TrimEnd());
 		}
 
 		public static ulong[] ProductFib(ulong prod)
 		{
-			var set = new List<ulong>();
-			set.AddRange(new ulong[] { 0, 1 });
-			for (ulong i = 2; i <= prod; i++)
+			ulong prev = 0;
+			ulong curr = 1;
+			ulong multiplication = 0;
+			while (prev * curr < prod)
 			{
-				set.Add(set[(int)i - 1] + set[(int)i - 2]);
+				ulong temp = curr;
+				curr += prev;
+				prev = temp;
+				multiplication = curr * prev;
 			}
-			var fibonacii = set.ToArray();
-
-			ulong factorOne;
-			ulong factorTwo;
-			var isValidFactors = false;
-			var result = new ulong[3];
-
-			for (int i = 0; i < fibonacii.Length; i++)
-			{
-				factorOne = fibonacii[i];
-				for (int k = 0; k < fibonacii.Length; k++)
-				{
-					factorTwo = fibonacii[k];
-					var productValue = factorOne * factorTwo;
-					if (productValue == prod)
-					{
-						result[0] = factorOne;
-						result[1] = factorTwo;
-						result[2] = 1;
-						return result;
-
-					}
-					else if (factorOne * factorTwo > prod)
-					{
-						break;
-					}
-				}
-			}
-
-			if (isValidFactors == false)
-			{
-				for (int i = 0; i < fibonacii.Length; i++)
-				{
-					var firstFactor = fibonacii[i];
-					var secondFactor = fibonacii[i + 1];
-					if (firstFactor * secondFactor > prod)
-					{
-						result[0] = firstFactor;
-						result[1] = secondFactor;
-						result[2] = 0;
-						return result;
-					}
-				}
-			}
-
-			return result;
+			var isFactorsOfProduct = multiplication == prod ? 1 : 0;
+			return new ulong[] { prev, curr, (ulong)isFactorsOfProduct };
 		}
 	}
 }
